74cd29bb4222ab7bb46dd05a0729f7558cd15d47
Anas Faris <anasfaris@yahoo.com>
2015-05-19 17:31:14 -0400

    Add link result

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,5 @@
 /** @jsx React.DOM */
 
-var data = [];
-
 var CreateBox = React.createClass({
 	handleCreateSubmit: function(link) {
 		$.ajax({
@@ -10,15 +8,12 @@ var CreateBox = React.createClass({
 			type: 'POST',
 			data: link,
 			success: function(data) {
-				this.setState({data: data});
+				$("#msg").html("<br>Link created. Visit link at <a target='_blank' href='/" + data.shortname + "'>http://refer.my/" + data.shortname + "</a>");
 			}.bind(this),
 			error: function(xhr, status, err) {
 				console.error('/shorten', status, err.toString());
 			}.bind(this)
 		});
-	},
-	getInitialState: function() {
-		return {data: []};
 	},
 	render: function() {
 		return (
@@ -26,30 +21,9 @@ var CreateBox = React.createClass({
 				<h1>Refer</h1>
 				<h2>I refer to great website with great contents</h2>
 				<CreateForm onCreateSubmit={this.handleCreateSubmit}/>
-				<CreateResult data={this.props.data} />
 			</div>
 		);
 	}
-});
-
-var CreateResult = React.createClass({
-	render: function() {
-		return (
-			<div className="createResult">
-				<Result short={this.props.data.short}></Result>
-			</div>
-		);
-	}
-});
-
-var Result = React.createClass({
-		render: function() {
-			return (
-				<div className="result">
-					<h3>{this.props.short}</h3>
-				</div>
-			);
-		}
 });
 
 var CreateForm = React.createClass({
@@ -70,14 +44,14 @@ var CreateForm = React.createClass({
 			<form className="createForm" onSubmit={this.handleSubmit}>
 				<div className="form-inline">
 					<p>
-						Insert url to shorten:
-						<input type="text" className="form-control" placeholder="Insert long url" ref="long"/>
+						URL:
+						<input type="text" className="form-control" placeholder="http://www.example.com" ref="long"/>
 					</p>
 				</div>
 				<div className="form-inline">
 					<p>
-						Shorten as: refer.my/
-						<input type="text" className="form-control" placeholder="Your short name" ref="short"/>
+						Shorten as: http://refer.my/
+						<input type="text" className="form-control" placeholder="shortname" ref="short"/>
 					</p>
 				</div>
 				<input type="submit" className="btn btn-default" value="Shorten" />
@@ -87,6 +61,6 @@ var CreateForm = React.createClass({
 });
 
 React.render(
-	<CreateBox data={data} />,
+	<CreateBox />,
 	document.getElementById('app')
 );
